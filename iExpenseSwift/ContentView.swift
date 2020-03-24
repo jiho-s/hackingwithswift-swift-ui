da66785213353cd14060c15724c2ad9b6234f23a
jiiiho <tlswlgh120@naver.com>
2020-03-24 13:25:27 +0900

    Initial Commit

---
+++ iExpenseSwift/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  iExpenseSwift
+//
+//  Created by 신지호 on 2020/03/24.
+//  Copyright © 2020 jiho. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
