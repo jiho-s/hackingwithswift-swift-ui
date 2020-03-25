974445b1732bdf5b55ee4476638c5de4a9e0face
jiiho <tlswlgh120@naver.com>
2020-03-25 20:01:37 +0900

    Working with Identifiable items in SwiftUI using UUID, unique identify

--- iExpenseSwift/ExpenseItem.swift
+++ iExpenseSwift/ExpenseItem.swift
@@ -5,8 +5,10 @@
 //  Created by 신지호 on 2020/03/25.
 //  Copyright © 2020 jiho. All rights reserved.
 //
-
-struct ExpenseItem {
+import SwiftUI
+// Identifiable 을 붙이면 ForEach에 id: \.id 부분을 없앨수 있다
+struct ExpenseItem: Identifiable {
+    let id = UUID()
     let name: String
     let type: String
     let amount: Int
