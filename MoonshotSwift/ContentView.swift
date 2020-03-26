56f4fb719ab6259c0cdc522db75d7f4b4ced4a5d
jiiiho <tlswlgh120@naver.com>
2020-03-26 12:14:35 +0900

    Initial Commit

---
+++ MoonshotSwift/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  MoonshotSwift
+//
+//  Created by 신지호 on 2020/03/26.
+//  Copyright © 2020 jiho. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
