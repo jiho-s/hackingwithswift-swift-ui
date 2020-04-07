7a22014d225045a3c2539cc62c2f15f332353972
jiiiho <tlswlgh120@naver.com>
2020-04-07 10:05:48 +0900

    Initial Commit

---
+++ BookwormSwift/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  BookwormSwift
+//
+//  Created by 신지호 on 2020/04/07.
+//  Copyright © 2020 jiho. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
