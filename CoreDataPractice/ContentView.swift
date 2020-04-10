c78da73ed2a78c27e4e764e5bf897f1ad38f6ab3
jiiiho <tlswlgh120@naver.com>
2020-04-10 10:06:44 +0900

    Initial Commit

---
+++ CoreDataPractice/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  CoreDataPractice
+//
+//  Created by 신지호 on 2020/04/10.
+//  Copyright © 2020 jiho. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
